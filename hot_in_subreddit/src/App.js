ec1d572a91ce6109bbe6a8286aa90c976e154dd7
Konstantin Andreev <chelvideo@mail.ru>
2020-10-16 15:42:11 +0500

    feat: add pagination

--- hot_in_subreddit/src/App.js
+++ hot_in_subreddit/src/App.js
@@ -5,6 +5,9 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import IconButton from '@material-ui/core/IconButton';
 import './App.css';
 
 function ListItemLink(props) {
@@ -21,30 +24,60 @@ function ListItemLink(props) {
 
 
 function App() {
-  const [curListing, changeCurListing] = useState([]);
+  const [curListing, changeCurListing] = useState({data: {children: []}});
+  const [page, changePage] = useState('');
 
-    async function getHotPosts(subreddit) {
+    async function getHotPosts(subreddit, params) {
       const url = `https://www.reddit.com/r/${subreddit}/hot.json`;
-      const resp = await fetch(url);
+      
+      const resp = await fetch(url + params);
       const data =  await resp.json();
-      changeCurListing(data.data.children);
-      console.log(data.data.children);
+      changeCurListing(data);
+      changePage(data.data.after);
+      console.log(data.data.after);
     }
     
 
   function inputSubreddit() {
     const subreddit = document.querySelector('#subreddit');
     console.log(subreddit.value);
-    getHotPosts(subreddit.value);
+    getHotPosts(subreddit.value, '');
     //changeCurListing(subreddit.value);
+  }
+
+  function nextListing() {
+    const subreddit = document.querySelector('#subreddit');
+    console.log(subreddit.value);
+    const params = `?after=${curListing.data.after}&count=25`;
+    getHotPosts(subreddit.value, params);
+  }
+
+  function prevListing() {
+    const subreddit = document.querySelector('#subreddit');
+    console.log(subreddit.value);
+    const params = `?before=${curListing.data.before}&count=25`;
+    getHotPosts(subreddit.value, params);
   }
 
   return (
     <div>
        <TextField id="subreddit" label="Subreddit" />
        <Button variant="contained" onClick={inputSubreddit}>Show hot</Button>
+
+       <label htmlFor="icon-button-file">
+          <IconButton color="primary" aria-label="upload picture" component="span">
+            <ArrowBackIosIcon onClick={prevListing}/>
+          </IconButton>
+        </label>
+
+        <label htmlFor="icon-button-file">
+          <IconButton color="primary" aria-label="upload picture" component="span">
+            <ArrowForwardIosIcon onClick={nextListing}/>
+          </IconButton>
+        </label>
+       
       <List>
-        {ListItemLink(curListing)}
+        {ListItemLink(curListing.data.children)}
       </List>
       
     </div>
