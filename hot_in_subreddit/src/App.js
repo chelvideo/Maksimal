41d824a250a435b1b1451a166cf33a2ba323a411
Konstantin Andreev <chelvideo@mail.ru>
2020-10-16 11:39:04 +0500

    feat: add Material UI

--- hot_in_subreddit/src/App.js
+++ hot_in_subreddit/src/App.js
@@ -1,5 +1,22 @@
 import React, { useState, useEffect } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
+
+function ListItemLink(props) {
+  return (
+    props.map(item => {
+      return (
+      <ListItem button component="a" href={item.data.url}>
+        <ListItemText primary={item.data.title} />
+      </ListItem>
+      )
+    })
+  );
+}
 
 
 function App() {
@@ -18,7 +35,10 @@ function App() {
 
   return (
     <div>
-      {curListing.map(item => item.data.title)}
+      <List>
+        {ListItemLink(curListing)}
+      </List>
+      
     </div>
   );
 }
